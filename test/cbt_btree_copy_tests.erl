43de84149645eb4608c25009dc3757e58aedf967
benoitc <bchesneau@gmail.com>
2014-06-19 15:20:51 +0200

    make sure we can copy compressed btrees.

--- test/cbt_btree_copy_tests.erl
+++ test/cbt_btree_copy_tests.erl
@@ -35,6 +35,18 @@ btree_copy_test_() ->
         }
     }.
 
+btree_copy_compressed_test_() ->
+    TNumItems = [50, 100, 300, 700, 811, 2333, 6594, 9999, 15003, 21477,
+                 38888, 66069, 150123, 420789, 711321],
+    {
+        "Copy Compressed BTree",
+        {
+            foreachx,
+            fun setup_copy/1, fun teardown/2,
+            [{N, fun  should_copy_compressed_btree/2} || N <- TNumItems]
+        }
+    }.
+
 should_copy_btree(NumItems, {ReduceFun, _OriginalFileName, _CopyFileName,
                              Fd, FdCopy}) ->
     KVs = [{I, I} || I <- lists:seq(1, NumItems)],
@@ -60,9 +72,41 @@ should_copy_btree(NumItems, {ReduceFun, _OriginalFileName, _CopyFileName,
         [], []),
     ?_assertMatch(KVs, lists:reverse(CopyKVs)).
 
+should_copy_compressed_btree(NumItems, {ReduceFun, _OriginalFileName,
+                                        _CopyFileName, Fd, FdCopy}) ->
+
+    KVs = [{I, I} || I <- lists:seq(1, NumItems)],
+    {ok, Btree} = make_btree(Fd, KVs, ReduceFun, snappy),
+
+    {_, Red, _} = cbt_btree:get_state(Btree),
+
+    CopyCallback = fun(KV, Acc) -> {KV, Acc + 1} end,
+    {ok, RootCopy, FinalAcc} = cbt_btree_copy:copy(
+        Btree, FdCopy, [{before_kv_write, {CopyCallback, 0}}]),
+
+    ?assertMatch(FinalAcc, length(KVs)),
+
+    {ok, BtreeCopy} = cbt_btree:open(
+        RootCopy, FdCopy, [{compression, snappy}, {reduce, ReduceFun}]),
+
+    %% check copy
+    {_, RedCopy, _} = cbt_btree:get_state(BtreeCopy),
+    ?assertMatch(Red, RedCopy),
+    {ok, _, CopyKVs} = cbt_btree:fold(
+        BtreeCopy,
+        fun(KV, _, Acc) -> {ok, [KV | Acc]} end,
+        [], []),
+    ?_assertMatch(KVs, lists:reverse(CopyKVs)).
+
+
+
 
 make_btree(Fd, KVs, ReduceFun) ->
-    {ok, Btree} = cbt_btree:open(nil, Fd, [{compression, none},
+    make_btree(Fd, KVs, ReduceFun, none).
+
+make_btree(Fd, KVs, ReduceFun, Compression) ->
+
+    {ok, Btree} = cbt_btree:open(nil, Fd, [{compression, Compression},
                                            {reduce, ReduceFun}]),
     {ok, Btree2} = cbt_btree:add_remove(Btree, KVs, []),
     {_, Red, _} = cbt_btree:get_state(Btree2),
